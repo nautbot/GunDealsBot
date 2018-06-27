3caa93255d6c8685fbfe192284ccc1e6f95a2e95
DEVxALMIGHTY <37675911+DEVxALMIGHTY@users.noreply.github.com>
2018-06-27 15:35:13 -0500

    clean up file - remove extranneous comments

--- EmbedField.py
+++ EmbedField.py
@@ -1,36 +1,34 @@
-"""
-This class is represents a single field that can be added to type Discord.Embed
-"""
+""
+"
+This class represents a single field that can be added to type Discord.Embed ""
+"
 
 class EmbedField:
     def __init__(self):
-        self._name = ''
+		self._name = ''
 		self._value = ''
 		self.inline = False
-		
-    @property
-    def name(self): # implements the get - this name is *the* name
-        return self._name
-    
-    @name.setter
-    def name(self, name): # name must be the same
-        self._name = name
-	
-	
+
 	@property
-    def value(self): # implements the get - this name is *the* name
-        return self._value
-    
-    @value.setter
-    def value(self, value): # name must be the same
-        self._value = value
-		
-		
+	def name(self):
+		return self._name
+
+	@name.setter
+	def name(self, name):
+		self._name = name
+
 	@property
-    def inline(self): # implements the get - this name is *the* name
-        return self._inline
-    
-    @value.setter
-    def inline(self, inline): # name must be the same
-        self._inline = inline
-    
+	def value(self):
+		return self._value
+
+	@value.setter
+	def value(self, value):
+		self._value = value
+
+	@property
+	def inline(self):
+		return self._inline
+
+	@value.setter
+	def inline(self, inline):
+		self._inline = inline
